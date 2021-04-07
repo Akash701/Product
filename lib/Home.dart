82c860ce449c695439c1bf158185dad92947ec49
Akash J Nair <akashjnair701@gmail.com>
2021-04-07 12:24:56 +0530

    Icons added

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:product/iconCard.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,18 +11,130 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(40),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search Product',
-                icon: Icon(Icons.search),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(15),
+                    child: TextField(
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                      decoration: InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey[300],
+                        hintText: 'Search Product',
+                        hintStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                        icon: Icon(Icons.search),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Icon(Icons.shopping_cart_outlined),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey[300],
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Stack(children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Icon(Icons.notifications_none_outlined),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey[300],
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    top: 0,
+                    child: Container(
+                      child: Center(
+                        child: Text(
+                          '3',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      width: 16,
+                      height: 16,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ]),
+                SizedBox(
+                  width: 10,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              padding: EdgeInsets.only(right: 150, top: 25),
+              child: Column(
+                children: [
+                  Text(
+                    'Summer Special Sale',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Expanded(
+                    child: Text(
+                      'CashBack 20%',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              width: 330,
+              height: 95,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+                shape: BoxShape.rectangle,
+                color: Colors.purple,
               ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 40,
+            ),
+            IconsCard(),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/iconCard.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+class IconsCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            children: [
+              Container(
+                width: 70,
+                height: 70,
+                // margin: EdgeInsets.only(left: 20),
+                child: Icon(
+                  Icons.lightbulb_outline_rounded,
+                  size: 40,
+                  color: Colors.white,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  shape: BoxShape.rectangle,
+                  color: Colors.deepOrangeAccent,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                // margin: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Flash Sale',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              )
+            ],
+          ),
+          Column(
+            children: [
+              Container(
+                width: 70,
+                height: 70,
+                // margin: EdgeInsets.only(left: 20),
+                child: Icon(
+                  Icons.sticky_note_2_outlined,
+                  size: 40,
+                  color: Colors.white,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  shape: BoxShape.rectangle,
+                  color: Colors.deepOrangeAccent,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                // margin: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Bill',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              )
+            ],
+          ),
+          Column(
+            children: [
+              Container(
+                width: 70,
+                height: 70,
+                // margin: EdgeInsets.only(left: 20),
+                child: Icon(
+                  Icons.videogame_asset_outlined,
+                  size: 40,
+                  color: Colors.white,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  shape: BoxShape.rectangle,
+                  color: Colors.deepOrangeAccent,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                // margin: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Game',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              )
+            ],
+          ),
+          Column(
+            children: [
+              Container(
+                width: 70,
+                height: 70,
+                // margin: EdgeInsets.only(left: 20),
+                child: Icon(
+                  Icons.card_giftcard_outlined,
+                  size: 40,
+                  color: Colors.white,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  shape: BoxShape.rectangle,
+                  color: Colors.deepOrangeAccent,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                // margin: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Gift',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
